8aa3f504b101a480a30533f18a4e8ebbd5a9628d
TSFDX <94276444+TSFDX@users.noreply.github.com>
2022-11-25 11:15:50 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+
+API_ID = os.getenv('API_ID','24146636')                            # You can get this from -->  https://my.telegram.org/auth  
+API_HASH = os.getenv('API_HASH','bad542d1995e2766f14cc3425320b5d0')               # You can get this from -->  https://my.telegram.org/auth
+BOT_TOKEN = os.getenv('BOT_TOKEN','5885028860:AAGagCLORbKmnVfOH-QZ_HSbSywcW8GJaWI')           # You can get this from --> @BotFather
+BOT_NAME = os.getenv('BOT_NAME','BOT MUSIK DOWNLOAD')           #Username of your bot
+GENIUS_API = os.getenv('GENIUS_API','hLWqhLON4MamWWEXoluQRsyaaHrUmj7wIi9y_GAMY1pRS8Vn855Kmnpx2IZziQU_')        # You can get this from --> https://docs.genius.com/
